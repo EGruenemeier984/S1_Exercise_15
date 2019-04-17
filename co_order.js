c6a7b620385e26d552b86863e5fa10a21a3cb153
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2019-04-17 15:23:31 -0700

    Created validation checks for the payment
    Used regex to compare text structures to then display custom validation message.

--- co_order.js
+++ co_order.js
@@ -29,35 +29,64 @@
 */
 
 window.addEventListener("load", function () {
-      var orderForm = document.forms.orderForm;
-      orderForm.elements.orderDate.value = new Date().toDateString();
-      orderForm.elements.model.focus();
+	var orderForm = document.forms.orderForm;
+	orderForm.elements.orderDate.value = new Date().toDateString();
+	orderForm.elements.model.focus();
+	calcOrder();
+
+	// event handlers for the web form
+	orderForm.elements.model.onchange = calcOrder;
+	orderForm.elements.qty.onchange = calcOrder;
+
+	var planOptions = document.querySelectorAll('input[name = "protection"]');
+	for (var i = 0; i < planOptions.length; i++) {
+		planOptions[i].onclick = calcOrder;
+	}
 });
-
-window.addEventListener("click", function () {
-      calcOrder();
-})
-
-window.addEventListener("change", function () {
-      calcOrder();
-});
-
 
 function calcOrder() {
-      var orderForm = document.forms.orderForm;
+	var orderForm = document.forms.orderForm;
 
-      // calculate the initial cost of the order
-      var mIndex = orderForm.elements.model.selectedIndex;
-      var mCost = orderForm.elements.model.options[mIndex].value;
-      var qIndex = orderForm.elements.qty.selectedIndex;
-      var quantity = orderForm.elements.qty[qIndex].value;
+	// calculate the initial cost of the order
+	var mIndex = orderForm.elements.model.selectedIndex;
+	var mCost = orderForm.elements.model.options[mIndex].value;
+	var qIndex = orderForm.elements.qty.selectedIndex;
+	var quantity = orderForm.elements.qty[qIndex].value;
 
 
-      // initial cost = model cost x quantity 
-      var initialCost = mCost * quantity;
-      orderForm.elements.initialCost.value = initialCost;
+	// initial cost = model cost x quantity 
+	var initialCost = mCost * quantity;
+	orderForm.elements.initialCost.value = formatUSACurrency(initialCost);
 
-      // retrieve the cost of the users protection plan 
-      var pCost = document.querySelector('input[name="protection]:checked]').value * quantity;
-      orderForm.elements.protectionCost.value = pCost;
+	// retrieve the cost of the users protection plan 
+	var pCost = document.querySelector('input[name="protection"]:checked').value * quantity;
+	orderForm.elements.protectionCost.value = formatNumber(pCost, 2);
+	// Calculate the order subtotal
+	orderForm.elements.subtotal.value = formatNumber(initialCost + pCost, 2);
+
+	// Calculate the 5% sales tax.
+	var salesTax = 0.05 * (initialCost + pCost);
+	orderForm.elements.salesTax.value = formatNumber(salesTax, 2);
+
+	// Calculate the total cost of the order
+	var totalCost = initialCost + pCost + salesTax;
+	orderForm.elements.totalCost.value = formatUSACurrency(totalCost);
+
+	// Store the order details 
+	orderForm.elements.modelName.value = orderForm.elements.model.options[mIndex].text;
+	orderForm.elements.protectionName.value = document.querySelector('input[name = "protection"]:checked').nextSibling.nodeValue;
+}
+
+function formatNumber(val, decimals) {
+	return val.toLocaleString(undefined, {
+		minimumFractionDigits: decimals,
+		maximumFractionDigits: decimals
+	});
+}
+
+function formatUSACurrency(val) {
+	return val.toLocaleString('en-us', {
+		style: "currency",
+		currency: "USD"
+	});
 }
